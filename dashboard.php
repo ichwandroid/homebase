d0695ad22f26aac63f94a4e36ec424e6f56981ef
Ichwan <ichwan.laruku@gmail.com>
2023-11-07 15:04:44 +0700

    ahjbs

--- dashboard.php
+++ dashboard.php
@@ -229,15 +229,15 @@ include 'connect.php'
                             </tr>
                         </thead>
                         <?php
-                         $data = mysqli_query($db, "SELECT * FROM tbl_prestasi");
+                         $data = mysqli_query($db, "SELECT * FROM tbl_prestasi INNER JOIN tbl_siswa on tbl_prestasi.nis = tbl_siswa.nis");
                          while ($a = mysqli_fetch_array($data)) {
                         ?>
                         <tbody>
                             <tr>
                                 <td><?php echo $a['timescan']?></td>
                                 <td><?php echo $a['nis']?></td>
-                                <td><?php echo $a['nis']?></td>
-                                <td><?php echo $a['nis']?></td>
+                                <td><?php echo $a['NAMA_LENGKAP']?></td>
+                                <td><?php echo $a['KELAS']?> - <?php echo $a['NAMA_KELAS']?></td>
                                 <td><?php echo $a['homebase']?></td>
                                 <td><?php echo $a['status']?></td>
                                 <td><?php echo $a['poin']?></td>
